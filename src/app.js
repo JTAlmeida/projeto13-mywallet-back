f4ae82e32725bbf51973ba6e961b6462d2207138
Jonatas <jonatasalmeidasm@gmail.com>
2022-09-08 10:26:20 -0300

    added post for sign-in and sign-up, already with validations

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
-import joi from "joi";
+import { authSignIn, authSignUp } from "./schemas/authSchemas.js";
 import cors from "cors";
 import dayjs from "dayjs";
 import { MongoClient, ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -25,10 +27,40 @@ mongoClient
     );
   });
 
-app.get("/", async (req, res) => {
+app.post("/auth/sign-in", async (req, res) => {
+  const user = req.body;
+
+  const validation = authSignIn.validate(user, { abortEarly: false });
+
+  if (validation.error) {
+    const err = validation.error.details.map((error) => {
+      return error.message;
+    });
+    return res.status(422).send({ message: err });
+  }
+
   try {
-    const data = await db.collection("user").find().toArray();
-    return res.send(data);
+    const checkUser = await db
+      .collection("users")
+      .findOne({ email: user.email });
+    const checkPassword = checkUser
+      ? bcrypt.compareSync(user.password, checkUser.password)
+      : false;
+    if (!checkUser || !checkPassword) {
+      return res
+        .status(422)
+        .send({ message: "Error: wrong email or password." });
+    }
+    await db.collection("sessions").deleteMany({ userId: checkUser._id });
+    const token = uuid();
+    await db.collection("sessions").insertOne({ token, userId: checkUser._id });
+    return res
+      .status(200)
+      .send({
+        message: `${checkUser.name} successfully logged in!`,
+        token,
+        user: { name: checkUser.name, email: checkUser.email },
+      });
   } catch (error) {
     return res.status(500).send({
       error: error.message,
@@ -37,9 +69,35 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/", async (req, res) => {
+app.post("/auth/sign-up", async (req, res) => {
+  const user = req.body;
+
+  const validation = authSignUp.validate(user, { abortEarly: false });
+
+  if (validation.error) {
+    const err = validation.error.details.map((error) => {
+      return error.message;
+    });
+    return res.status(422).send({ message: err });
+  }
+
   try {
-    await db.collection("user").insertOne(req.body);
+    const checkEmail = await db
+      .collection("users")
+      .findOne({ email: user.email });
+    if (checkEmail) {
+      return res
+        .status(409)
+        .send({ message: "This email is already being used." });
+    }
+
+    const passwordHash = bcrypt.hashSync(user.password, 10);
+
+    await db.collection("users").insertOne({
+      name: user.name,
+      email: user.email,
+      password: passwordHash,
+    });
     return res.sendStatus(201);
   } catch (error) {
     return res.status(500).send({
@@ -49,4 +107,6 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(5000);
+app.listen(5000, () => {
+  console.log("Listening on port 5000.");
+});
