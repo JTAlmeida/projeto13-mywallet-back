3305fea438c0b0492bcb858116f7d62a7cf042c3
Jonatas <jonatasalmeidasm@gmail.com>
2022-09-08 08:59:22 -0300

    added db connection and a test route on port 5000

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import joi from "joi";
 import cors from "cors";
 import dayjs from "dayjs";
-import { MongoClient, ObjectId} from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,6 +10,43 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
+let db;
+
+mongoClient
+  .connect()
+  .then(() => {
+    db = mongoClient.db("mywallet");
+  })
+  .catch((error) => {
+    return console.error(
+      `Error "${error}" while trying to connect to database.`
+    );
+  });
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await db.collection("user").find().toArray();
+    return res.send(data);
+  } catch (error) {
+    return res.status(500).send({
+      error: error.message,
+      hint: "It's better to check if database is properly connected.",
+    });
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    await db.collection("user").insertOne(req.body);
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.status(500).send({
+      error: error.message,
+      hint: "It's better to check if database is properly connected.",
+    });
+  }
+});
 
 app.listen(5000);
